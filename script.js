ab241bd25f307f0b14795abed4e6fb8dea073cc9
Justin-Yun <juyu9703@colorado.edu>
2025-02-26 21:33:02 -0700

    Removed Tailwind CSS

--- script.js
+++ script.js
@@ -8,29 +8,4 @@ document.addEventListener("DOMContentLoaded", () => {
             text.style.opacity = "1";
         }, 500);
     }
-
-    // Lightbox functionality
-    const lightbox = document.getElementById("lightbox");
-    const lightboxImg = document.getElementById("lightbox-img");
-    const closeBtn = document.getElementById("close-lightbox");
-
-    document.querySelectorAll(".gallery-image").forEach((img) => {
-        img.addEventListener("click", function () {
-            lightboxImg.src = this.src;
-            lightbox.classList.remove("hidden");
-        });
-    });
-
-    function closeLightbox() {
-        lightbox.classList.add("hidden");
-    }
-
-    closeBtn.addEventListener("click", closeLightbox);
-    lightbox.addEventListener("click", (event) => {
-        if (event.target !== lightboxImg) closeLightbox();
-    });
-
-    document.addEventListener("keydown", (event) => {
-        if (event.key === "Escape") closeLightbox();
-    });
 });
